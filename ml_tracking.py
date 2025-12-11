5fd5183f32f91e40c7463ba4443b520347da5180
Ashwini012000 <Ashwini.s-katageri@hp.com>
2025-12-11 16:10:08 +0530

    mlflow running locally

--- ml_tracking.py
+++ ml_tracking.py
@@ -20,7 +20,7 @@ def no_emoji_print(self, run_id):
 ml_client.TrackingServiceClient._log_url = no_emoji_print
 
 # Set MLflow to local folder (VERY IMPORTANT)
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_tracking_uri("mlruns\517387353629759616\46cd37d6501b4ed2ba3748bfd8624469")
 
 # Set experiment name (CREATE IF NOT EXISTS)
 mlflow.set_experiment("Student_Grade_Project")
